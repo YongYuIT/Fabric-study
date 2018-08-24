a06c40e3d19a34f75717c7e0c0f2213bc7d2d64e
yong <yong.yu@hsditech.com>
2018-08-24 15:17:39 +0800

    test sdk

--- 3-orgs-test/main/main.go
+++ 3-orgs-test/main/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"os"
+	"path/filepath"
+	"../my_test"
+)
 
 func main() {
-	fmt.Println("fuck you")
+	file, err := exec.LookPath(os.Args[0])
+	if err == nil {
+		path, err := filepath.Abs(file)
+		if err == nil {
+			fmt.Println(path)
+		}
+	}
+	my_test.Call_with_yong()
 }
