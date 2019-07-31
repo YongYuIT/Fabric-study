cf5abe13633f651b0697d6df22089173ea1975e0
YongYuIT <1198749126@qq.com>
2019-07-31 02:00:32 -0700

    zk and kafka built success

---
+++ hello-kafka/src/main/resources/kafka-cmd/kafka_1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin" >> ~/.bashrc
+source ~/.bashrc
+mkdir -p /tmp/kafka-logs
+cd /kafka/bin
+ln -sf bash /bin/sh
+rm -rf /kafka/config/server.properties
+./kafka-server-start.sh -daemon /kafka-cmd/server_1.properties
+/bin/bash
