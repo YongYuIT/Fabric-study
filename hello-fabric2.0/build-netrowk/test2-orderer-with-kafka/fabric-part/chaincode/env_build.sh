5e2e54b018b2b7bd0edca1f4083b3f9ae0f26adf
yong <1198749126@qq.com>
2020-04-26 10:31:40 +0800

    kafka集群启动完毕，下一步将结合kafka和fabric

---
+++ hello-fabric2.0/build-netrowk/test2-orderer-with-kafka/fabric-part/chaincode/env_build.sh
@@ -0,0 +1,10 @@
+export CORE_PEER_ADDRESS_1=peer0.thk_org1.thinking.com:7051
+export CORE_PEER_LOCALMSPID_1=THKOrg1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE_1=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/thk_org1.thinking.com/peers/peer0.thk_org1.thinking.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH_1=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/thk_org1.thinking.com/users/Admin\@thk_org1.thinking.com/msp
+export CORE_PEER_ADDRESS_2=peer0.thk_org2.thinking.com:10051
+export CORE_PEER_LOCALMSPID_2=THKOrg2MSP
+export CORE_PEER_TLS_ROOTCERT_FILE_2=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/thk_org2.thinking.com/peers/peer0.thk_org2.thinking.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH_2=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/thk_org2.thinking.com/users/Admin\@thk_org2.thinking.com/msp
+export ORDERER_ADDRESS=thinking_orderer.thinking.com:7050
+export ORDERER_CA_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/thinking.com/orderers/thinking_orderer.thinking.com/msp/tlscacerts/tlsca.thinking.com-cert.pem
---
+++ hello-fabric2.0/build-netrowk/test2-orderer-with-kafka/zk-kafka-part/kafka-cmd/kafka_2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin" >> ~/.bashrc
+source ~/.bashrc
+mkdir -p /tmp/kafka-logs
+cd /kafka/bin
+ln -sf bash /bin/sh
+rm -rf /kafka/config/server.properties
+./kafka-server-start.sh -daemon /kafka-cmd/server_2.properties
+/bin/bash
