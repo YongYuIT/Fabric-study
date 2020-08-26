5126509d5e4f710a4466f0e944c1a969fcce53be
yong <1198749126@qq.com>
2020-08-26 05:45:30 -0700

    failed with sample

--- fabric-k8s/build-with-ansible/test_yml/src/main/java/Main.java
+++ fabric-k8s/build-with-ansible/test_yml/src/main/java/Main.java
@@ -1,30 +1,24 @@
-import yml_any.AnyConfigTx;
-
-import static docker.BuildTools.buildOrderer;
-import static docker.BuildTools.buildPeer;
 import static msp.BuildTools.buildOrdererOrgs;
 import static msp.BuildTools.buildPeerOrgs;
-import static gen_and_channel.BuildTools.buildChannel;
-import static gen_and_channel.BuildTools.buildOrdererGenesis;
 import static yml_any.AnyConfigTx.AnyTx;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AnyTx();
+        //AnyTx();
         System.out.println("-------------------------------------");
         buildPeerOrgs();
         System.out.println("-------------------------------------");
         buildOrdererOrgs();
         System.out.println("-------------------------------------");
-        buildOrdererGenesis();
+        //buildOrdererGenesis();
         System.out.println("-------------------------------------");
-        buildChannel();
+        //buildChannel();
         System.out.println("-------------------------------------");
-        buildOrderer("1.4.6", "orderer.example.com");
+        //buildOrderer("1.4.6", "orderer.example.com");
         System.out.println("-------------------------------------");
-        buildPeer("1.4.6", "Org1MSP", "peer0.org1.example.com", 7051, "peer1.org1.example.com:8051");
-        buildPeer("1.4.6", "Org2MSP", "peer0.org2.example.com", 9051, "peer1.org2.example.com:10051");
-        buildPeer("1.4.6", "Org3MSP", "peer0.org3.example.com", 11051, "peer1.org3.example.com:12051");
+        //buildPeer("1.4.6", "Org1MSP", "peer0.org1.example.com", 7051, "peer1.org1.example.com:8051");
+        //buildPeer("1.4.6", "Org2MSP", "peer0.org2.example.com", 9051, "peer1.org2.example.com:10051");
+        //buildPeer("1.4.6", "Org3MSP", "peer0.org3.example.com", 11051, "peer1.org3.example.com:12051");
     }
 
 }
